daae3d1c9775bfb2aec1b3f3c85af82694873a96
Jeremiah <jherberg88@me.com>
2025-03-31 23:31:07 -0400

    add index template JS and CSS

---
+++ static/script.js
@@ -0,0 +1,9 @@
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text)
+        .then(() => {
+            alert("Copied to clipboard: " + text);
+        })
+        .catch(err => {
+            console.error("Failed to copy: ", err);
+        });
+}
